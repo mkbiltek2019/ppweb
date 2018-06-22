0a7125e3d9ccb75546764e6499170bc744f00d9a
Igor Gašparović <dragan.stanojevic@3cad.hr>
2018-06-22 15:32:36 +0200

    Load chart bug fix.

--- app/assets/js/charts.js
+++ app/assets/js/charts.js
@@ -1,6 +1,6 @@
 ﻿/*!
 charts.js
-(c) 2017 IG PROG, www.igprog.hr
+(c) 2018 IG PROG, www.igprog.hr
 */
 angular.module('charts', [])
 
@@ -44,39 +44,8 @@ angular.module('charts', [])
                 datasetOverride: dso
             }
         },
-        'createGuageChart': function (recommendedEnergy, energy) {
-            var data = google.visualization.arrayToDataTable([
-                  ['Label', 'Value'],
-                  ['kcal', 80]
-            ]);
-
-            var maxValue = recommendedEnergy + (recommendedEnergy * 0.2);
-
-            var options = {
-                title: "Energetska vrijednost",
-                min: 0, max: maxValue.toFixed(0),
-                greenFrom: recommendedEnergy - (recommendedEnergy * 0.02),
-                greenTo: recommendedEnergy + (recommendedEnergy * 0.02),
-                yellowFrom: recommendedEnergy + (recommendedEnergy * 0.02),
-                yellowTo: recommendedEnergy + (recommendedEnergy * 0.04),
-                redFrom: recommendedEnergy + (recommendedEnergy * 0.04),
-                redTo: maxValue,
-                minorTicks: 5
-            };
-            data.setValue(0, 1, energy);
-            var chart = new google.visualization.Gauge(document.getElementById('energyChart'));
-
-            chart.draw(data, options);
-
-            var chart1 = new google.visualization.Gauge(document.getElementById('energyChart1'));
-            chart1.draw(data, options);
-            var chart2 = new google.visualization.Gauge(document.getElementById('energyChart2'));
-            chart2.draw(data, options);
-            var chart3 = new google.visualization.Gauge(document.getElementById('energyChart3'));
-            chart3.draw(data, options);
-
-        },
         'guageChart': function (id, value, unit, options) {
+            if (google.visualization === undefined || document.getElementById(id) == null) { return false; }
             var data = google.visualization.arrayToDataTable([
                   ['Label', 'Value'],
                   [unit, 80]
